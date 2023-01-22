d292dba5968df0eb1987e7656b17134a88ea8e97
Vidit Khandelwal <viditk17@gmail.com>
2023-01-22 14:09:47 +0530

    fixed light mode issues

--- src/components/NewsItem.tsx
+++ src/components/NewsItem.tsx
@@ -11,9 +11,9 @@ export default function NewsItem(props: { url: string | UrlObject; title: string
         className="flex w-full flex-col space-y-1 hover:bg-zinc-700 p-2 rounded-xl col-span-2 font-light h-max"
       >
         <div className="flex flex-col space-y-1 ">
-          <p className="text-md text-zinc-200 text-2xl">{props.title}</p>
-          <p className="text-zinc-400">{props.abstract}</p>
-          <div className="flex h-max flex-row space-x-1 text-zinc-300 text-xs font-medium items-center ">
+          <p className="text-md text-zinc-900 dark:text-zinc-200 text-2xl">{props.title}</p>
+          <p className="text-zinc-700 dark:text-zinc-400">{props.abstract}</p>
+          <div className="flex h-max flex-row space-x-1 text-zinc-800 dark:text-zinc-300 text-xs font-medium items-center ">
             <p className="">
               {new Date(props.time).toLocaleTimeString("en-IN", {
                 timeStyle: "short",
